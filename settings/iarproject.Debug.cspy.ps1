4c8bad3803511e2adfc8477b510714b6846ccc41
Ekaterina <70802069+katyachalykh@users.noreply.github.com>
2020-11-29 17:47:26 +0500

    1

--- settings/iarproject.Debug.cspy.ps1
+++ settings/iarproject.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Desktop\Work05-main\labtim2\settings\iarproject.Debug.general.xcl" --backend -f "C:\Users\mircc\Desktop\Work05-main\labtim2\settings\iarproject.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Documents\GitHub\Lab5\settings\iarproject.Debug.general.xcl" --backend -f "C:\Users\mircc\Documents\GitHub\Lab5\settings\iarproject.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Desktop\Work05-main\labtim2\settings\iarproject.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\mircc\Desktop\Work05-main\labtim2\settings\iarproject.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Documents\GitHub\Lab5\settings\iarproject.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\mircc\Documents\GitHub\Lab5\settings\iarproject.Debug.driver.xcl" 
 }
